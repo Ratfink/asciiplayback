e5193c2cc1279cab904f05bcd70f9cdc2210df1d
Clayton G. Hobbs <clay@lakeserv.net>
2014-06-16 12:50:17 -0400

    More work on the ASCIImator Desktop mockup
    Let's face it, right now all I have is a mockup.

--- src/asciimator_desktop.py
+++ src/asciimator_desktop.py
@@ -2,6 +2,7 @@
 import sys
 from gi.repository import Gtk, Gio, Gdk, GObject
 from asciiplayback import *
+from gtkasciiplayer import *
 
 class ASCIImatorDesktop(Gtk.Window):
     def __init__(self):
@@ -10,150 +11,156 @@ class ASCIImatorDesktop(Gtk.Window):
 
         if len(sys.argv) > 1:
             self.filename = sys.argv[1]
-            self.player = ASCIIPlayback(filename=self.filename, speed=0)
+            self.asciimation = ASCIImation(filename=self.filename)
+            self.player = ASCIIPlayback(self.asciimation, speed=0)
         else:
             self.filename = ""
-            blank_asciimation = ASCIImation(font_family='monospace', size=[15, 3])
-            blank_asciimation.frames.append(Frame(text='\nNo file loaded!\n'))
-            self.player = ASCIIPlayback(asciimation=blank_asciimation, speed=0)
+            self.asciimation = ASCIImation(font_family='monospace', size=[15, 3])
+            self.asciimation.frames.append(Frame(text='\nNo file loaded!\n'))
+            self.player = ASCIIPlayback(asciimation=self.asciimation, speed=0)
 
-#        print('\n'.join(Gtk.IconTheme().list_icons()))
+        print('\n'.join(Gtk.IconTheme().list_icons()))
 
-        hb = Gtk.HeaderBar()
-        hb.props.show_close_button = True
-        hb.props.title = "ASCIImator Desktop"
+        self.hsize_group = Gtk.SizeGroup(Gtk.SizeGroupMode.HORIZONTAL)
+
+        right_box = self.main_content()
+        left_box = self.sidebar()
+        separator = Gtk.Separator(orientation=Gtk.Orientation.VERTICAL)
+
+        hb = self.headerbar()
         self.set_titlebar(hb)
+
+        box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        box.pack_start(left_box, False, False, 0)
+        box.pack_start(separator, False, False, 0)
+        box.pack_start(right_box, True, True, 0)
+
+        self.add(box)
+
+    def headerbar(self):
+        header = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+
+        left_header = Gtk.HeaderBar()
+        left_header.props.show_close_button = True
+        right_header = Gtk.HeaderBar()
+        right_header.props.show_close_button = True
+
+        left_header.get_style_context().add_class("titlebar")
+        left_header.get_style_context().add_class("titlebar-left")
+        right_header.get_style_context().add_class("titlebar")
+        right_header.get_style_context().add_class("titlebar-right")
+
+        layout_desc = Gtk.Settings.get_default().props.gtk_decoration_layout
+        tokens = layout_desc.split(":", 2)
+        if tokens != None:
+            right_header.props.decoration_layout = ":" + tokens[1]
+            left_header.props.decoration_layout = tokens[0]
+
+        self.title = Gtk.Label(self.filename)
+        self.title.get_style_context().add_class("title")
+        right_header.set_custom_title(self.title)
 
         button = Gtk.Button(image=Gtk.Image.new_from_gicon(Gio.ThemedIcon(
                             name="document-save-symbolic"),
                             Gtk.IconSize.BUTTON))
-        hb.pack_end(button)
+        right_header.pack_end(button)
         button = Gtk.Button(image=Gtk.Image.new_from_gicon(Gio.ThemedIcon(
                             name="document-open-symbolic"),
                             Gtk.IconSize.BUTTON))
         button.connect("clicked", self.do_open)
-        hb.pack_end(button)
-        
+        right_header.pack_end(button)
 
+        left_header.props.title = "ASCIImator Desktop"
+        left_header.props.subtitle = "Offline ASCII Animator"
+
+        header.pack_start(left_header, False, False, 0)
+        header.pack_start(Gtk.Separator(orientation=Gtk.Orientation.VERTICAL),
+                          False, False, 0)
+        header.pack_start(right_header, True, True, 0)
+        
+        self.hsize_group.add_widget(left_header)
+
+        return header
+
+    def sidebar(self):
+        left = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+
+        ab = Gtk.ActionBar()
+        ab.set_center_widget(self.stack_switcher)
+        left.pack_end(ab, False, False, 0)
+
+
+
+        self.hsize_group.add_widget(left)
+
+        return left
+
+    def main_content(self):
         stack = Gtk.Stack()
-#        stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT_RIGHT)
-#        stack.set_transition_duration(250)
-        
-        edit = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        stack.add_titled(edit, "edit", "Edit")
+        stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT_RIGHT)
+        stack.set_transition_duration(250)
 
+        self.stack_switcher = Gtk.StackSwitcher()
+        self.stack_switcher.set_stack(stack)
+
+        #### Edit panel ####
+        edit = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+
+        edit_window = Gtk.ScrolledWindow()
         label = Gtk.Label()
         label.set_markup("<big>Edit coming soon...</big>")
-        edit.pack_start(label, True, True, 0)
+        edit_window.add(label)
+        edit.pack_start(edit_window, True, True, 0)
 
         ab = Gtk.ActionBar()
-        stack_switcher = Gtk.StackSwitcher()
-        stack_switcher.set_stack(stack)
-        ab.pack_start(stack_switcher)
-        edit.pack_end(ab, False, False, 0)
-
-        preview = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-
-        labelbox = Gtk.Box()
-        label = Gtk.Label()
-        label.set_alignment(0, 0)
-        label.set_width_chars(self.player.asciimation.size[0])
-        label.set_max_width_chars(self.player.asciimation.size[0])
-        labelbox.pack_start(label, True, False, 0)
-        preview.pack_start(labelbox, True, False, 0)
-        self.do_animate(label)
-
-        ab = Gtk.ActionBar()
-
-        stack_switcher = Gtk.StackSwitcher()
-        stack_switcher.set_stack(stack)
-        ab.pack_start(stack_switcher)
-
         ab_buttons = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         Gtk.StyleContext.add_class(ab_buttons.get_style_context(), "linked")
 
-        btn_previous = Gtk.Button(image=Gtk.Image.new_from_gicon(Gio.ThemedIcon(
-                                  name="media-skip-backward-symbolic"),
-                                  Gtk.IconSize.BUTTON))
-        btn_previous.connect("clicked", self.do_previous)
-        ab_buttons.add(btn_previous)
-
-        btn_rewind = Gtk.Button(image=Gtk.Image.new_from_gicon(Gio.ThemedIcon(
-                                name="media-seek-backward-symbolic"),
-                                Gtk.IconSize.BUTTON))
-        btn_rewind.connect("clicked", self.do_rewind)
-        ab_buttons.add(btn_rewind)
-
-        btn_play = Gtk.Button(image=Gtk.Image.new_from_gicon(Gio.ThemedIcon(
-                              name="media-playback-start-symbolic"),
-                              Gtk.IconSize.BUTTON))
-        btn_play.connect("clicked", self.do_play)
-        ab_buttons.add(btn_play)
-
-        btn_forward = Gtk.Button(image=Gtk.Image.new_from_gicon(Gio.ThemedIcon(
-                                 name="media-seek-forward-symbolic"),
-                                 Gtk.IconSize.BUTTON))
-        btn_forward.connect("clicked", self.do_forward)
-        ab_buttons.add(btn_forward)
-
-        btn_next = Gtk.Button(image=Gtk.Image.new_from_gicon(Gio.ThemedIcon(
-                              name="media-skip-forward-symbolic"),
-                              Gtk.IconSize.BUTTON))
-        btn_next.connect("clicked", self.do_next)
-        ab_buttons.add(btn_next)
-
+        button = Gtk.Button(image=Gtk.Image.new_from_gicon(Gio.ThemedIcon(
+                            name="edit-undo-symbolic"),
+                            Gtk.IconSize.BUTTON))
+        ab_buttons.add(button)
+        button = Gtk.Button(image=Gtk.Image.new_from_gicon(Gio.ThemedIcon(
+                            name="edit-redo-symbolic"),
+                            Gtk.IconSize.BUTTON))
+        ab_buttons.add(button)
+        button = Gtk.Button(image=Gtk.Image.new_from_gicon(Gio.ThemedIcon(
+                            name="list-add-symbolic"),
+                            Gtk.IconSize.BUTTON))
+        ab_buttons.add(button)
+        button = Gtk.Button(image=Gtk.Image.new_from_gicon(Gio.ThemedIcon(
+                            name="list-remove-symbolic"),
+                            Gtk.IconSize.BUTTON))
+        ab_buttons.add(button)
+        button = Gtk.Button(image=Gtk.Image.new_from_gicon(Gio.ThemedIcon(
+                            name="edit-copy-symbolic"),
+                            Gtk.IconSize.BUTTON))
+        ab_buttons.add(button)
+        button = Gtk.Button(image=Gtk.Image.new_from_gicon(Gio.ThemedIcon(
+                            name="edit-paste-symbolic"),
+                            Gtk.IconSize.BUTTON))
+        ab_buttons.add(button)
         ab.set_center_widget(ab_buttons)
+
+        edit.pack_end(ab, False, False, 0)
+
+        stack.add_titled(edit, "edit", "Edit")
+
+        #### Preview panel ####
+        preview = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+
+        self.video = GtkASCIIPlayer(self.player)
+        preview.pack_start(self.video, True, False, 0)
+
+        ab = Gtk.ActionBar()
+
+        self.controls = GtkASCIIControls(self.player)
+        ab.set_center_widget(self.controls)
         preview.pack_end(ab, False, False, 0)
 
         stack.add_titled(preview, "preview", "Preview")
-        
-        self.add(stack)
 
-        stack_switcher = Gtk.StackSwitcher()
-        stack_switcher.set_stack(stack)
-        hb.pack_start(stack_switcher)
-
-    def do_animate(self, widget):
-        asciiframe = self.player.next_frame()
-
-        # Process the string to make each line the correct length, and ensure
-        # that there is exactly the right number of lines.  Perhaps this
-        # should be somewhere in the model code...
-        text = []
-        for line in asciiframe.text.split('\n'):
-            text.append("{{: <{}s}}".format(self.player.asciimation.size[0]).format(line))
-            text[-1] = text[-1][:self.player.asciimation.size[0]]
-        while len(text) < self.player.asciimation.size[1]:
-            text.append(' '*self.player.asciimation.size[0])
-        text = '\n'.join(text[:self.player.asciimation.size[1]])
-
-        # Draw the string and background
-        widget.set_markup('<span size="{0}" foreground="{1}"\
- font_family="{2},monospace">{3}</span>'.format(self.player.asciimation.font_size*1024,
-                          asciiframe.foreground_color,
-                          self.player.asciimation.font_family, text))
-        widget.modify_bg(Gtk.StateType.NORMAL, Gdk.Color.parse(asciiframe.background_color)[1])
-
-        GObject.timeout_add(self.player.asciimation.speed, self.do_animate, widget)
-
-    def do_previous(self, button):
-        self.player.to_start()
-
-    def do_rewind(self, button):
-        self.player.rewind()
-
-    def do_play(self, button):
-        self.player.toggle_playing()
-        if self.player.speed == 0:
-            button.set_image(Gtk.Image.new_from_gicon(Gio.ThemedIcon(name="media-playback-start-symbolic"), Gtk.IconSize.BUTTON))
-        else:
-            button.set_image(Gtk.Image.new_from_gicon(Gio.ThemedIcon(name="media-playback-pause-symbolic"), Gtk.IconSize.BUTTON))
-
-    def do_forward(self, button):
-        self.player.fast_forward()
-
-    def do_next(self, button):
-        self.player.to_end()
+        return stack
 
     def do_open(self, button):
         dialog = Gtk.FileChooserDialog("Open", self,
@@ -166,8 +173,11 @@ class ASCIImatorDesktop(Gtk.Window):
         response = dialog.run()
         if response == Gtk.ResponseType.OK:
             self.filename = dialog.get_filename()
+            self.title.set_text(self.filename)
             self.asciimation = ASCIImation(filename=self.filename)
             self.player = ASCIIPlayback(asciimation=self.asciimation, speed=0)
+            self.video.player = self.player
+            self.controls.player = self.player
         elif response == Gtk.ResponseType.CANCEL:
             pass
 
@@ -187,4 +197,26 @@ class ASCIImatorDesktop(Gtk.Window):
 win = ASCIImatorDesktop()
 win.connect("delete-event", Gtk.main_quit)
 win.show_all()
+
+style_provider = Gtk.CssProvider()
+
+css = b"""
+.titlebar-left:dir(ltr),
+.titlebar-left:dir(rtl) {
+    border-top-right-radius: 0;
+}
+
+.titlebar-right:dir(ltr),
+.titlebar-right:dir(rtl) {
+    border-top-left-radius: 0;
+}
+"""
+
+style_provider.load_from_data(css)
+
+Gtk.StyleContext.add_provider_for_screen(
+    Gdk.Screen.get_default(),
+    style_provider,
+    Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+)
 Gtk.main()
