3be8dcc58e37fb6e5be846982cef285e625ed497
Clayton G. Hobbs <clay@lakeserv.net>
2014-06-16 08:52:34 -0400

    Split the GTK player into view and controller
    Now there's a widget for the view (GtkASCIIPlayer) and a widget for
    the controller (GtkASCIIControls).  It's super nice this way because I
    can do the layout in ASCIImator Desktop however I want while reusing a
    large chunk of code from the GTK player.

--- src/asciiplaybackgtk.py
+++ src/asciiplaybackgtk.py
@@ -3,6 +3,7 @@ import sys
 from gi.repository import Gtk, Gio, Gdk, GObject
 from asciiplayback import *
 from asciimation import *
+from gtkasciiplayer import *
 
 class ASCIIPlaybackGtk(Gtk.Window):
     def __init__(self):
@@ -34,96 +35,16 @@ class ASCIIPlaybackGtk(Gtk.Window):
 
         box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
 
-        labelbox = Gtk.Box()
-        label = Gtk.Label()
-        label.set_alignment(0, 0)
-        label.set_width_chars(self.player.asciimation.size[0])
-        label.set_max_width_chars(self.player.asciimation.size[0])
-        labelbox.pack_start(label, True, False, 0)
-        box.pack_start(labelbox, True, False, 0)
-        self.do_animate(label)
+        self.video = GtkASCIIPlayer(self.player)
+        box.pack_start(self.video, True, True, 0)
 
         ab = Gtk.ActionBar()
 
-        ab_buttons = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-        Gtk.StyleContext.add_class(ab_buttons.get_style_context(), "linked")
-
-        btn_previous = Gtk.Button(image=Gtk.Image.new_from_gicon(Gio.ThemedIcon(
-                                  name="media-skip-backward-symbolic"),
-                                  Gtk.IconSize.BUTTON))
-        btn_previous.connect("clicked", self.do_previous)
-        ab_buttons.add(btn_previous)
-
-        btn_rewind = Gtk.Button(image=Gtk.Image.new_from_gicon(Gio.ThemedIcon(
-                                name="media-seek-backward-symbolic"),
-                                Gtk.IconSize.BUTTON))
-        btn_rewind.connect("clicked", self.do_rewind)
-        ab_buttons.add(btn_rewind)
-
-        btn_play = Gtk.Button(image=Gtk.Image.new_from_gicon(Gio.ThemedIcon(
-                              name="media-playback-pause-symbolic"),
-                              Gtk.IconSize.BUTTON))
-        btn_play.connect("clicked", self.do_play)
-        ab_buttons.add(btn_play)
-
-        btn_forward = Gtk.Button(image=Gtk.Image.new_from_gicon(Gio.ThemedIcon(
-                                 name="media-seek-forward-symbolic"),
-                                 Gtk.IconSize.BUTTON))
-        btn_forward.connect("clicked", self.do_forward)
-        ab_buttons.add(btn_forward)
-
-        btn_next = Gtk.Button(image=Gtk.Image.new_from_gicon(Gio.ThemedIcon(
-                              name="media-skip-forward-symbolic"),
-                              Gtk.IconSize.BUTTON))
-        btn_next.connect("clicked", self.do_next)
-        ab_buttons.add(btn_next)
-
-        ab.set_center_widget(ab_buttons)
+        self.controls = GtkASCIIControls(self.player)
+        ab.set_center_widget(self.controls)
         box.pack_end(ab, False, False, 0)
 
         self.add(box)
-
-    def do_animate(self, widget):
-        asciiframe = self.player.next_frame()
-
-        # Process the string to make each line the correct length, and ensure
-        # that there is exactly the right number of lines.  Perhaps this
-        # should be somewhere in the model code...
-        text = []
-        for line in asciiframe.text.split('\n'):
-            text.append("{{: <{}s}}".format(self.player.asciimation.size[0]).format(line))
-            text[-1] = text[-1][:self.player.asciimation.size[0]]
-        while len(text) < self.player.asciimation.size[1]:
-            text.append(' '*self.player.asciimation.size[0])
-        text = '\n'.join(text[:self.player.asciimation.size[1]])
-
-        # Draw the string and background
-        widget.set_markup('<span size="{0}" foreground="{1}"\
- font_family="{2},monospace">{3}</span>'.format(self.player.asciimation.font_size*1024,
-                          asciiframe.foreground_color,
-                          self.player.asciimation.font_family, text))
-        widget.modify_bg(Gtk.StateType.NORMAL, Gdk.Color.parse(asciiframe.background_color)[1])
-
-        GObject.timeout_add(self.player.asciimation.speed, self.do_animate, widget)
-
-    def do_previous(self, button):
-        self.player.to_start()
-
-    def do_rewind(self, button):
-        self.player.rewind()
-
-    def do_play(self, button):
-        self.player.toggle_playing()
-        if self.player.speed == 0:
-            button.set_image(Gtk.Image.new_from_gicon(Gio.ThemedIcon(name="media-playback-start-symbolic"), Gtk.IconSize.BUTTON))
-        else:
-            button.set_image(Gtk.Image.new_from_gicon(Gio.ThemedIcon(name="media-playback-pause-symbolic"), Gtk.IconSize.BUTTON))
-
-    def do_forward(self, button):
-        self.player.fast_forward()
-
-    def do_next(self, button):
-        self.player.to_end()
 
     def do_open(self, button):
         dialog = Gtk.FileChooserDialog("Open", self,
@@ -137,6 +58,8 @@ class ASCIIPlaybackGtk(Gtk.Window):
         if response == Gtk.ResponseType.OK:
             self.filename = dialog.get_filename()
             self.player = ASCIIPlayback(ASCIImation(filename=self.filename))
+            self.video.player = self.player
+            self.controls.player = self.player
         elif response == Gtk.ResponseType.CANCEL:
             pass
 
---
+++ src/gtkasciiplayer.py
@@ -0,0 +1,99 @@
+from gi.repository import Gtk, Gio, Gdk, GObject
+from asciiplayback import *
+from asciimation import *
+
+class GtkASCIIPlayer(Gtk.Box):
+    def __init__(self, player):
+        Gtk.Box.__init__(self, orientation=Gtk.Orientation.VERTICAL)
+
+        self.player = player
+
+        labelbox = Gtk.Box()
+        label = Gtk.Label()
+        label.set_alignment(0, 0)
+        label.set_width_chars(self.player.asciimation.size[0])
+        label.set_max_width_chars(self.player.asciimation.size[0])
+        labelbox.pack_start(label, True, False, 0)
+        self.pack_start(labelbox, True, False, 0)
+        self.do_animate(label)
+
+    def do_animate(self, widget):
+        asciiframe = self.player.next_frame()
+
+        # Process the string to make each line the correct length, and ensure
+        # that there is exactly the right number of lines.  Perhaps this
+        # should be somewhere in the model code...
+        text = []
+        for line in asciiframe.text.split('\n'):
+            text.append("{{: <{}s}}".format(self.player.asciimation.size[0]).format(line))
+            text[-1] = text[-1][:self.player.asciimation.size[0]]
+        while len(text) < self.player.asciimation.size[1]:
+            text.append(' '*self.player.asciimation.size[0])
+        text = '\n'.join(text[:self.player.asciimation.size[1]])
+
+        # Draw the string and background
+        widget.set_markup('<span size="{0}" foreground="{1}"\
+ font_family="{2},monospace">{3}</span>'.format(self.player.asciimation.font_size*1024,
+                          asciiframe.foreground_color,
+                          self.player.asciimation.font_family, text))
+        widget.modify_bg(Gtk.StateType.NORMAL, Gdk.Color.parse(asciiframe.background_color)[1])
+
+        GObject.timeout_add(self.player.asciimation.speed, self.do_animate, widget)
+
+class GtkASCIIControls(Gtk.Box):
+    def __init__(self, player):
+        Gtk.Box.__init__(self, orientation=Gtk.Orientation.HORIZONTAL)
+        Gtk.StyleContext.add_class(self.get_style_context(), "linked")
+
+        self.player = player
+
+        btn_previous = Gtk.Button(image=Gtk.Image.new_from_gicon(Gio.ThemedIcon(
+                                  name="media-skip-backward-symbolic"),
+                                  Gtk.IconSize.BUTTON))
+        btn_previous.connect("clicked", self.do_previous)
+        self.add(btn_previous)
+
+        btn_rewind = Gtk.Button(image=Gtk.Image.new_from_gicon(Gio.ThemedIcon(
+                                name="media-seek-backward-symbolic"),
+                                Gtk.IconSize.BUTTON))
+        btn_rewind.connect("clicked", self.do_rewind)
+        self.add(btn_rewind)
+
+        btn_play = Gtk.Button(image=Gtk.Image.new_from_gicon(Gio.ThemedIcon(
+                              name="media-playback-pause-symbolic"),
+                              Gtk.IconSize.BUTTON))
+        btn_play.connect("clicked", self.do_play)
+        self.add(btn_play)
+
+        btn_forward = Gtk.Button(image=Gtk.Image.new_from_gicon(Gio.ThemedIcon(
+                                 name="media-seek-forward-symbolic"),
+                                 Gtk.IconSize.BUTTON))
+        btn_forward.connect("clicked", self.do_forward)
+        self.add(btn_forward)
+
+        btn_next = Gtk.Button(image=Gtk.Image.new_from_gicon(Gio.ThemedIcon(
+                              name="media-skip-forward-symbolic"),
+                              Gtk.IconSize.BUTTON))
+        btn_next.connect("clicked", self.do_next)
+        self.add(btn_next)
+
+    def do_previous(self, button):
+        self.player.to_start()
+
+    def do_rewind(self, button):
+        self.player.rewind()
+
+    def do_play(self, button):
+        self.player.toggle_playing()
+        if self.player.speed == 0:
+            button.set_image(Gtk.Image.new_from_gicon(Gio.ThemedIcon(
+                name="media-playback-start-symbolic"), Gtk.IconSize.BUTTON))
+        else:
+            button.set_image(Gtk.Image.new_from_gicon(Gio.ThemedIcon(
+                name="media-playback-pause-symbolic"), Gtk.IconSize.BUTTON))
+
+    def do_forward(self, button):
+        self.player.fast_forward()
+
+    def do_next(self, button):
+        self.player.to_end()
